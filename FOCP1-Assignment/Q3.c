0e78640791d050d94c809da572d573b8246cf13b
parthkhullar07-prog <parthkhullar07@gmail.com>
2025-10-24 17:17:04 +0530

    Q3.c

--- FOCP1-Assignment/Q3.c
+++ FOCP1-Assignment/Q3.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter First Number");
+    scanf("%d",&a);
+    printf("Enter Second Number");
+    scanf("%d",&b);
+    c=a+(~b+1);
+    printf("The Difference is %d",c);
+    return 0;
+}
