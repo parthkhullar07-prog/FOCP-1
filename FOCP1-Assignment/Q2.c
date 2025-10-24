31a617ce6ab3c882a6bb42b78c9b11f72769a084
parthkhullar07-prog <parthkhullar07@gmail.com>
2025-10-24 17:08:25 +0530

    Q2.c

--- FOCP1-Assignment/Q2.c
+++ FOCP1-Assignment/Q2.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main(){
+    int a,b,c=0;
+    printf("Enter First Number");
+    scanf("%d",&a);
+    printf("Enter Second Number");
+    scanf("%d",&b);
+    for(int i=1 ; i<=a && i<=b ; i++){
+        if(a%i==0 && b%i==0 && i>c){
+            c=i;
+        }
+    }
+    printf("HCF is %d",c);
+    return 0;
+}
