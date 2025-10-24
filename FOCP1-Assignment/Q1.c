51712dc6a35857e29c24c9dc1753580073539406
parthkhullar07-prog <parthkhullar07@gmail.com>
2025-10-24 16:55:14 +0530

    Q1.c
    Implement check for Armstrong numbers in C.

--- FOCP1-Assignment/Q1.c
+++ FOCP1-Assignment/Q1.c
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int a,b,c,d=0;
+    double e=0.0;
+    printf("Enter Number");
+    scanf("%d",&a);
+    b=a;
+    while(a!=0){
+        a=a/10;
+        d=d+1;
+    }
+    a=b;
+    while(a!=0){
+        c=a%10;
+        e=e+pow(c,d);
+        a=a/10;
+    }
+    if((int)e==b){
+        printf("Armstrong Number %d",b);
+    }
+    else{
+        printf("Not Armstrong Number %d",b);
+    }
+    return 0;
+}
